ad6d39e4e67c8f15cb9feabd82088f4f8fabaebc
Andre Marcilio <andremarcilio@jera.com.br>
2023-08-27 17:38:53 -0300

    [NEW] implemented github_repos data layer

---
+++ lib/modules/github_repos/adapters/github_repos_adapter.dart
@@ -0,0 +1,16 @@
+import '../model/github_repo.dart';
+
+abstract class GithubRepoAdapter {
+  static GithubRepo fromMap(dynamic map) {
+    return GithubRepo(
+      id: map['id'] as int,
+      size: map['size'] as int,
+      title: map['name'] as String,
+      description: map['description'] ?? '',
+    );
+  }
+
+  static List<GithubRepo> fromMapList(List maps) {
+    return maps.map(GithubRepoAdapter.fromMap).toList();
+  }
+}
