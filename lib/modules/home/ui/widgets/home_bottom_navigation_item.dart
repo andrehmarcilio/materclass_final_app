a27399679e08e609c8ff2350d10c7c3a4d649dd8
Andre Marcilio <andremarcilio@jera.com.br>
2023-09-03 17:14:38 -0300

    [NEW] created a custom bottom_navigation_widget

---
+++ lib/modules/home/ui/widgets/home_bottom_navigation_item.dart
@@ -0,0 +1,11 @@
+import '../../../../commons/models/svg_asset.dart';
+
+class HomeBottomNavigationItem {
+  final String label;
+  final SvgAsset svgAsset;
+
+  const HomeBottomNavigationItem({
+    required this.label,
+    required this.svgAsset,
+  });
+}
